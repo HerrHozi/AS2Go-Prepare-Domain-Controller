73f3447657c23b23c229f7fba678b953b0a06ccd
Holger Zimmermann <96825160+HerrHozi@users.noreply.github.com>
2022-11-02 10:42:30 +0100

    Update New-AS2GoUsers.ps1

--- New-AS2GoUsers.ps1
+++ New-AS2GoUsers.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-Create four accounts (Victim, Helpdesk (2x) & Domain Admin) for the attack demo.
+Create four accounts (Victim, Helpdesk, Service Account & Domain Admin) for the attack demo.
 
 .DESCRIPTION
 
@@ -9,8 +9,8 @@ These accounts are needed for the attack.
 
 .NOTES
 
-last update: 2022-10-30
-File Name  : New-AS2GoUsers.ps1 | Version 2.5.5
+last update: 2022-11-01
+File Name  : New-AS2GoUsers.ps1 | Version 2.5.6
 Author     : Holger Zimmermann | @HerrHozi
 https://herrhozi.com
 
@@ -32,20 +32,18 @@ Create an VI-xxxxx Victim account. "y" is the default.
 
 .EXAMPLE
 PS> cd C:\Temp\AS2Go
-PS> .\AS2GO-create-users.ps1
+PS> .\New-AS2GoUsers.ps1
 
 .EXAMPLE
-PS> .\AS2GO-create-users.ps1 -Shortname n
+PS> .\New-AS2GoUsers.ps1 -Shortname n
 
 .EXAMPLE
-PS> .\AS2GO-create-users.ps1 -Shortname HerrHoZi
-
+PS> .\New-AS2GoUsers.ps1 -Shortname HerrHoZi
 
 .EXAMPLE
-PS> .\AS2GO-create-users.ps1 -DomainAdmin y -HelpDesk y -Victim n -Shortname HerrHoZi
+PS> .\New-AS2GoUsers.ps1 -DomainAdmin y -HelpDesk y -Victim n -Shortname HerrHoZi
+
 creates the only the domain admin & help desk account with an dedicated name, DA-HerrHoZi & HD-HerrHoZi
-
-
 
 .LINK
 https://herrhozi.com
@@ -64,19 +62,18 @@ $scriptName  = $MyInvocation.MyCommand.Name
 $logfile     = "$PoSHPath\$scriptName.log"
 $NTDSDITFILE = "$PoSHPath\ntds.dit"
 
-
 #get the Domain Roout OU
+Import-Module ActiveDirectory
 $ADRoot = Get-ADRootDSE
 $Domain = Get-ADDomain
 $RootOU = $ADRoot.defaultNamingContext
 
 
  $AS2GoUser = @(
-       [pscustomobject]@{Path = "OU=Accounts,OU=Tier 0 Assets,OU=AS2Go,$RootOU"; ComputerPath = "OU=Tier 0 Servers,OU=Tier 0 Assets,OU=AS2Go,$RootOU"}
-       [pscustomobject]@{Path = "OU=Accounts,OU=Tier 1 Assets,OU=AS2Go,$RootOU"; ComputerPath = "OU=Devices,OU=Tier 1 Assets,OU=AS2Go,$RootOU"}
-       [pscustomobject]@{Path = "OU=Accounts,OU=Tier 2 Assets,OU=AS2Go,$RootOU"; ComputerPath = "OU=Devices,OU=Tier 2 Assets,OU=AS2Go,$RootOU"}
+       [pscustomobject]@{SVCAccounts = "OU=Service Accounts,OU=Tier 0 Assets,OU=AS2Go,$RootOU";Path = "OU=Accounts,OU=Tier 0 Assets,OU=AS2Go,$RootOU"; ComputerPath = "OU=Tier 0 Servers,OU=Tier 0 Assets,OU=AS2Go,$RootOU"}
+       [pscustomobject]@{SVCAccounts = "OU=Service Accounts,OU=Tier 1 Assets,OU=AS2Go,$RootOU";Path = "OU=Accounts,OU=Tier 1 Assets,OU=AS2Go,$RootOU"; ComputerPath = "OU=Devices,OU=Tier 1 Assets,OU=AS2Go,$RootOU"}
+       [pscustomobject]@{SVCAccounts = "OU=Service Accounts,OU=Tier 2 Assets,OU=AS2Go,$RootOU";Path = "OU=Accounts,OU=Tier 2 Assets,OU=AS2Go,$RootOU"; ComputerPath = "OU=Devices,OU=Tier 2 Assets,OU=AS2Go,$RootOU"}
    )
-
 
 # OU Path for new Users
 $sUPNSuffix = "@HerrHozi.com"
@@ -88,19 +85,21 @@ $HDSecurePass = ConvertTo-SecureString -String $master'hd' -AsPlainText -Force
 $VISecurePass = ConvertTo-SecureString -String $master'vi' -AsPlainText -Force
 
 # Manager's must exist in the AD
-$DAManager = (get-aduser -filter * | Get-Random).samaccountname # "Holger"
-$HDManager = (get-aduser -filter * | Get-Random).samaccountname # "HoZi"
-$VIManager = (get-aduser -filter * | Get-Random).samaccountname #"Zimbo"
+$DAManager = (get-aduser -filter * | Get-Random).samaccountname
+$HDManager = (get-aduser -filter * | Get-Random).samaccountname
+$VIManager = (get-aduser -filter * | Get-Random).samaccountname
 
 # User thumbnail Photo
-$DAPhoto = "$PoSHPath\mdi-admin.jpg"
-$HDPhoto = "$PoSHPath\mdi-helpdesk.jpg"
-$VIPhoto = "$PoSHPath\mdi-victim.jpg"
+$DAPhoto = "$PoSHPath\As2Go-admin.jpg"
+$HDPhoto = "$PoSHPath\As2Go-helpdesk.jpg"
+$VIPhoto = "$PoSHPath\As2Go-victim.jpg"
 
 # Active Directory Security Groups
-$VIGroup = "SG-AS2Go-Victims"            # member of local admins on VICTIM PC
-$HDGroup = "SG-AS2Go-HelpDesk"           # member of local admins on ADMIN PC & VICTIM PC
-$DAGroup = "SG-AS2Go-Admins"             # member of local admins on ADMIN PC
+$VIGroup       = "SG-AS2Go-Victims"            # member of local admins on VICTIM PC
+$HDGroup       = "SG-AS2Go-HelpDesk"           # member of local admins on ADMIN PC & VICTIM PC
+$DAGroup       = "SG-AS2Go-Admins"             # member of local admins on ADMIN PC
+$DomainAdmins  = (Get-ADGroup -Filter * | where {($_.SID -like "*-512")}).name
+$ProtectedUser = (Get-ADGroup -Filter * | where {($_.SID -like "*-525")}).name
 
 #define the user first & last name
 $sFirstName = Get-Date -Format HHmmssff  # create the first name based on hours, minutes, seconds & milli seconds
@@ -112,11 +111,7 @@ $TimeSpan = New-TimeSpan -Days 7 -Hours 0 -Minutes 0
 #Service to create SPN
 $services = ("HTTPS","FTP","CIFS","kafka","MSSQL","POP3","HTTP")
 
-Import-Module ActiveDirectory
-
-
-Function NewMDIUser()
-
+Function New-AG2GoUserAccount()
 {
 
 param([string] $sUserPrincipalName, 
@@ -171,11 +166,10 @@ else
 }
 
 
-
 $sNewUserPrincipalName = ($sNewName + $sUPNSuffix)
 
 
-Write-Host "`nStart creating 3 users for use case '$sNewName'. Accounts expire after $TimeSpan days`n" -ForegroundColo Yellow
+Write-Host "`nStart creating 4 users for use case '$sNewName'. Accounts expire after $TimeSpan days`n" -ForegroundColo Yellow
 
 
 if ($Victim -eq 'y')
@@ -190,7 +184,7 @@ if ($Victim -eq 'y')
   $bthumbnailPhoto     = $VIPhoto
   $sPath               = $AS2GoUser[2].Path
 
-  NewMDIUser -sUserPrincipalName $sUserPrincipalName -sName $sName -sSamaccountName $sSamAccountName -sFirstName $sFirstName -sLastname $sLastname -sDisplayName $sDisplayName -sPath $sPath -secure_string_pwd $VISecurePass
+  New-AG2GoUserAccount -sUserPrincipalName $sUserPrincipalName -sName $sName -sSamaccountName $sSamAccountName -sFirstName $sFirstName -sLastname $sLastname -sDisplayName $sDisplayName -sPath $sPath -secure_string_pwd $VISecurePass
   $sName = $sSamAccountName
 
   Add-ADGroupMember -Identity $VIGroup -Members $sName
@@ -212,7 +206,7 @@ if ($HelpDesk -eq 'y')
   $sPath               = $AS2GoUser[1].Path
 
   
-  NewMDIUser -sUserPrincipalName $sUserPrincipalName -sName $sName -sSamaccountName $sSamAccountName -sFirstName $sFirstName -sLastname $sLastname -sDisplayName $sDisplayName -sPath $sPath -secure_string_pwd $HDSecurePass
+  New-AG2GoUserAccount -sUserPrincipalName $sUserPrincipalName -sName $sName -sSamaccountName $sSamAccountName -sFirstName $sFirstName -sLastname $sLastname -sDisplayName $sDisplayName -sPath $sPath -secure_string_pwd $HDSecurePass
   $sName = $sSamAccountName
   Add-ADGroupMember -Identity $HDGroup  -Members $sName
   Set-ADUser $sName -Replace @{thumbnailPhoto=([byte[]](Get-Content $bthumbnailPhoto -Encoding byte))} -Manager $HDManager -Initials "HD" -Title "Helpdesk" -Department "Tier 1"
@@ -222,23 +216,22 @@ if ($HelpDesk -eq 'y')
   # additionally Helpdesk User (HD-HerrHoziP)
   # member of the protected users group
     
-  $sUserPrincipalName  = "HD-" + $sNewName + "P" + $sUPNSuffix
-  $sName               = $sNewName + "P-HD"
-  $sSamAccountName     = "HD-" + $sNewName + "P"
-  $sDisplayName        = "Helpdesk User ($sSamAccountName)"
+  $sUserPrincipalName  = "SVC-" + $sNewName + $sUPNSuffix
+  $sName               = $sNewName + "-SVC"
+  $sSamAccountName     = "SVC-" + $sNewName
+  $sDisplayName        = "Service Account ($sSamAccountName)"
   $bthumbnailPhoto     = $HDPhoto
-  $sPath               = $AS2GoUser[1].Path
+  $sPath               = $AS2GoUser[0].SVCAccounts
 
-  NewMDIUser -sUserPrincipalName $sUserPrincipalName -sName $sName -sSamaccountName $sSamAccountName -sFirstName $sFirstName -sLastname $sLastname -sDisplayName $sDisplayName -sPath $sPath -secure_string_pwd $HDSecurePass
+  New-AG2GoUserAccount -sUserPrincipalName $sUserPrincipalName -sName $sName -sSamaccountName $sSamAccountName -sFirstName $sFirstName -sLastname $sLastname -sDisplayName $sDisplayName -sPath $sPath -secure_string_pwd $HDSecurePass
   $sName = $sSamAccountName
-  $ProtectedUser =  (Get-ADGroup -Filter * -Properties * | where {($_.SID -like "*-525")}).name
-  Add-ADGroupMember -Identity $ProtectedUser -Members $sName
-  Add-ADGroupMember -Identity $HDGroup  -Members $sName
-  Set-ADUser $sName -Replace @{thumbnailPhoto=([byte[]](Get-Content $bthumbnailPhoto -Encoding byte))} -Manager $HDManager -Initials "HD" -Title "Helpdesk" -Department "Tier 1"
-  Write-Host "... created new user - $sName | Helpdesk User"
+    Add-ADGroupMember -Identity $ProtectedUser -Members $sName
+  Add-ADGroupMember -Identity $DomainAdmins  -Members $sName
+  Set-ADUser $sName -Replace @{thumbnailPhoto=([byte[]](Get-Content $bthumbnailPhoto -Encoding byte))} -Manager $HDManager -Initials "HD" -Title "Helpdesk" -Department "Tier 0"
+  Write-Host "... created new user - $sName | Service Account"
   
   #create new computer
-  $NewComputer = "PAW-$sNewName"
+  $NewComputer = "SRV-$sNewName"
   New-ADComputer -Name $NewComputer -Description "needed for Kerberoasting Attack" -Path $AS2GoUser[1].ComputerPath  -Location $sNewName -OperatingSystem "Windows 10 Enterprise"
 
   #set ServicePrincipalNames to a random service
@@ -261,10 +254,9 @@ if ($DomainAdmin -eq 'y')
   $bthumbnailPhoto     = $DAPhoto
   $sPath               = $AS2GoUser[0].Path
 
-  NewMDIUser -sUserPrincipalName $sUserPrincipalName -sName $sName -sSamaccountName $sSamAccountName -sFirstName $sFirstName -sLastname $sLastname -sDisplayName $sDisplayName -sPath $sPath -secure_string_pwd $DASecurePass
+  New-AG2GoUserAccount -sUserPrincipalName $sUserPrincipalName -sName $sName -sSamaccountName $sSamAccountName -sFirstName $sFirstName -sLastname $sLastname -sDisplayName $sDisplayName -sPath $sPath -secure_string_pwd $DASecurePass
 
   $sName = $sSamAccountName
-  $DomainAdmins =  (Get-ADGroup -Filter * -Properties * | where {($_.SID -like "*-512")}).name
   Add-ADGroupMember -Identity $DomainAdmins -Members $sName
   Add-ADGroupMember -Identity $DAGroup -Members $sName
 
@@ -272,7 +264,7 @@ if ($DomainAdmin -eq 'y')
   Write-Host "... created new user - $sName | Domain Admin"
 
   #create new computer object
-  $NewComputer = "SRV-$sNewName"
+  $NewComputer = "PAW-$sNewName"
   New-ADComputer -Name $NewComputer -Description "needed for Kerberoasting Attack" -Path $AS2GoUser[0].ComputerPath  -Location $sNewName -OperatingSystem "Windows Server 2019 Standard"
   
   #set ServicePrincipalNames to a random service
@@ -286,8 +278,6 @@ if ($DomainAdmin -eq 'y')
 
 # SUMMMARY
 # ========
-
-#$sNewName = "paul06"
 $attributesU = @("samaccountname","servicePrincipalName","name","canonicalName","department")
 $attributesC = @("samaccountname","servicePrincipalName","name","canonicalName","description")
  
@@ -297,24 +287,17 @@ Write-Host     "========================================" -ForegroundColor Yello
 Get-ADComputer -LDAPFilter "(sAMAccountName=*-$sNewName*)" -Properties $attributesC | select $attributesC | ft
 Get-ADUser     -LDAPFilter "(sAMAccountName=*$sNewName*)"   -Properties $attributesU | select $attributesU | ft
 
-#$sNewName = "20221027"
-
-$NewAS2GoUsers = Get-ADUser -LDAPFilter "(sAMAccountName=*$sNewName*)" 
-Foreach ($trustee in $NewAS2GoUsers)
+$NewUserAccounts = Get-ADUser -LDAPFilter "(sAMAccountName=*$sNewName*)" 
+Foreach ($trustee in $NewUserAccounts)
 {
   Write-Host $trustee.samAccountname "is now member of the following groups:" -ForegroundColor Yellow
   Get-ADPrincipalGroupMembership -Identity $trustee.samAccountname | ft name, GroupCategory, GroupScope, sid
 }
 
-
-
-
-
 $MyScript = $MyInvocation.MyCommand.Definition
 $OnServer = " on server [" + [Environment]::machinename + "]" 
 $byUser   = " by user [" + [Environment]::UserName + "]"
 $UseCase  = " Usecase [" + $sNewName + "]"
-
 
 Write-Host "`n`nReminder:" -ForegroundColor Yellow
 Write-Host "If you changed the default password, do NOT forget to update the XML file!!!!!!!" -ForegroundColor Yellow
@@ -324,7 +307,6 @@ Write-Host "If you changed the default password, do NOT forget to update the XML
 " " | Out-File -FilePath $logfile -Append -Encoding default 
 (Get-Date).ToString() + " last run: " + $MyScript + $onserver + $byUser + $UseCase  | Out-File -FilePath $logfile -Append -Encoding default 
 Get-ADUser -LDAPFilter "(sAMAccountName=*$sNewName)" -Properties canonicalName, Created  | select sAMAccountName, Created, userPrincipalName, name, canonicalName | ft | Out-File -FilePath $logfile -Append -Encoding default
-
 
 # update the dummy NTDS.DIT file
 # ==============================
